f4a01cb79f298eab84b3e45fd2110034662ca71f
rizza123 <59152445+rizza123@users.noreply.github.com>
2022-03-13 18:35:18 +1100

    Update main.lua

--- loader/main.lua
+++ loader/main.lua
@@ -1,5 +1,5 @@
 if game.GameId == "537413528"
   loadstring(game:HttpGet('https://raw.githubusercontent.com/rizza123/IllIlIlllIl-1/main/babft/main.lua'))()
   else
-  game.Players.LocalPlayer:kick("BABFT: Error - Must Be on Magenta Team. try find server with magenta team open") 
+  game.Players.LocalPlayer:kick("Unknown Game") 
 end
