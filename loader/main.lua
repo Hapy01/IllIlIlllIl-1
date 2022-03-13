dceceef3962ff7addbecadb6d2b2b14330e68ef4
rizza123 <59152445+rizza123@users.noreply.github.com>
2022-03-13 18:31:34 +1100

    Create main.lua

---
+++ loader/main.lua
@@ -0,0 +1,5 @@
+if game.GameId == "537413528"
+  loadstring(game:HttpGet('https://raw.githubusercontent.com/rizza123/IllIlIlllIl-1/main/babft/main.lua'))()
+  else
+  game.Players.LocalPlayer:kick("BABFT: Error - Must Be on Magenta Team. try find server with magenta team open") 
+end
