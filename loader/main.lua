e4dac6ccdb2405eeaac34842d48362ee64f8bf92
rizza123 <59152445+rizza123@users.noreply.github.com>
2022-03-13 18:44:59 +1100

    Update main.lua

--- loader/main.lua
+++ loader/main.lua
@@ -1,4 +1,4 @@
-if game.GameId == "537413528" then
+if game.GameId == 537413528 then
   loadstring(game:HttpGet('https://raw.githubusercontent.com/rizza123/IllIlIlllIl-1/main/babft/main.lua'))()
   else
   game.Players.LocalPlayer:kick("Unknown Game") 
